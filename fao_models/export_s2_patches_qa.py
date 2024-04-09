91557d9de4f4754f7fc158095756485368a54453
kyle-woodward <kw.geospatial@gmail.com>
2024-04-09 17:27:07 -0400

    test a 'least cloudy image' patch method alternative

---
+++ fao_models/export_s2_patches_qa.py
@@ -0,0 +1,191 @@
+import ee
+import os
+import google.auth
+from serving import *
+import os
+import argparse
+
+# os.environ['TF_ENABLE_ONEDNN_OPTS=0']
+
+PROJECT = "pc530-fao-fra-rss"  # change to your cloud project name
+# ee.Initialize(project=PROJECT)
+
+## INIT WITH HIGH VOLUME ENDPOINT
+credentials, _ = google.auth.default()
+ee.Initialize(
+    credentials,
+    project=PROJECT,
+    opt_url="https://earthengine-highvolume.googleapis.com",
+)
+
+# USE HEXAGONS TO MAKE PATCH BOUNDS ######################################################
+all_hex = ee.FeatureCollection(
+    "projects/pc530-fao-fra-rss/assets/reference/hexWCenPropertiesTropics"
+)
+# print(all_hex.size().getInfo())
+# print(all_hex.limit(1).getInfo()['features'][0]['properties'])
+
+hexForest = all_hex.filter(
+    ee.Filter.And(ee.Filter.eq("FOREST", 1), ee.Filter.eq("LU18CEN", "Forest"))
+)
+# print('pureForest size',hexForest.size().getInfo())
+# print('LU18CEN values pure forest',hexForest.aggregate_histogram('LU18CEN').getInfo())
+
+hexNonForest = all_hex.filter(
+    ee.Filter.And(ee.Filter.eq("FOREST", 0), ee.Filter.neq("LU18CEN", "Forest"))
+)
+# print('pureNonForest size',hexNonForest.size().getInfo())
+# print('LU18CEN values pure nonForest',hexNonForest.aggregate_histogram('LU18CEN').getInfo())
+
+# hexForest = hexForest.randomColumn().limit(10000,'random',False)
+# hexNonForest = hexNonForest.randomColumn().limit(10000,'random',False)
+FNFhex = hexForest.merge(hexNonForest)
+sample_size_total = FNFhex.size().getInfo()
+# print(sample_size_total)
+
+
+# create 320 x 320 m box for image patches (32x32 px patches for training)
+def hex_patch_box(fc, size):
+    def per_hex(f):
+        centroid = f.geometry().centroid()
+        patch_box = centroid.buffer(size / 2).bounds()
+        return ee.Feature(patch_box)
+
+    return fc.map(per_hex)
+
+
+patch_boxes = hex_patch_box(FNFhex, 320)
+
+# Finally, for actual workflow at botom we only need the centroids of each hexagon to generate the image patchess
+FNFhex_centroids = FNFhex.map(lambda h: ee.Feature(h.geometry().centroid()))
+# print(FNFhex_centroids.first().getInfo())
+
+# image patch generation from hexagon centroid
+hexLabel = (
+    ee.Image(0).paint(hexForest, 1).paint(hexNonForest, 2).selfMask().rename("class")
+)
+
+## QA -- Certain Region to export patches in ############################################
+central_african_republic = ee.FeatureCollection("FAO/GAUL/2015/level0").filterMetadata("ADM0_NAME", "equals", "Central African Republic").geometry()
+brazil = ee.FeatureCollection("FAO/GAUL/2015/level0").filterMetadata("ADM0_NAME", "equals", "Brazil").geometry()
+
+## MAKE S2 COMPOSITE IN HEXAGONS ##########################################
+# Using Cloud Score + for cloud/cloud-shadow masking
+# Harmonized Sentinel-2 Level 2A collection.
+s2 = ee.ImageCollection("COPERNICUS/S2_SR_HARMONIZED")
+
+# Cloud Score+ image collection. Note Cloud Score+ is produced from Sentinel-2
+# Level 1C data and can be applied to either L1C or L2A collections.
+csPlus = ee.ImageCollection("GOOGLE/CLOUD_SCORE_PLUS/V1/S2_HARMONIZED")
+
+# Use 'cs' or 'cs_cdf', depending on your use case; see docs for guidance.
+QA_BAND = "cs_cdf"
+
+# The threshold for masking; values between 0.50 and 0.65 generally work well.
+# Higher values will remove thin clouds, haze & cirrus shadows.
+CLEAR_THRESHOLD = 0.50
+
+# Make a clear median composite.
+sampleImage = (
+    s2.filterDate("2017-01-01", "2019-12-31")
+    .linkCollection(csPlus, [QA_BAND])
+    .map(lambda img: img.updateMask(hexLabel))
+    .map(lambda img: img.updateMask(img.select(QA_BAND).gte(CLEAR_THRESHOLD)))
+    .median()
+    .addBands(hexLabel)
+    .select(["B4", "B3", "B2", "B8", "class"], ["R", "G", "B", "N", "class"])
+)  # B G R classlabel
+
+# we pass entire S2 collection to the patch writer func 
+s2Coll = (
+    s2.filterDate("2018-01-01", "2018-12-31")
+    .linkCollection(csPlus, [QA_BAND])
+    .map(lambda img: img.updateMask(hexLabel))
+    .map(lambda img: img.updateMask(img.select(QA_BAND).gte(CLEAR_THRESHOLD)))
+    .map(lambda img: img.addBands(hexLabel))
+    .select(["B4", "B3", "B2", "B8", "class"], ["R", "G", "B", "N", "class"])
+)
+## TESTING ################################################################
+def main():
+    # initalize new cli parser
+    parser = argparse.ArgumentParser(description="Export S2 image patches.")
+
+    parser.add_argument(
+        "-o",
+        "--output_dir",
+        type=str,
+        help="path to config file",
+    )
+
+    parser.add_argument(
+        "-f",
+        "--forest",
+        dest="forest",
+        action="store_true",
+        help="export forest labeled patches",
+        required=False,
+    )
+
+    parser.add_argument(
+        "-nf",
+        "--nonforest",
+        dest="nonforest",
+        action="store_true",
+        help="export nonforest labeled patches",
+        required=False,
+    )
+    args = parser.parse_args()
+
+    parser.set_defaults(forest=False)
+    parser.set_defaults(nonforest=False)
+
+    if not os.path.exists(args.output_dir):
+        os.makedirs(args.output_dir)
+
+    # we have about a 1/3 to 2/3 split of forest / nonforest makeup of total hexagons
+    # set seed so we can reproduce list and skip exports that have finished.
+    seed = 42
+    ee_points_forest = (
+        hexForest
+        .filterBounds(brazil)
+        .map(lambda h: ee.Feature(h.geometry().centroid()))
+        .randomColumn(seed=seed)
+        .sort("random").limit(5)
+    )
+    ee_points_nonforest = (
+        hexNonForest
+        .filterBounds(brazil)
+        .map(lambda h: ee.Feature(h.geometry().centroid()))
+        .randomColumn(seed=seed)
+        .sort("random").limit(5)
+    )
+
+    if not args.forest and not args.nonforest:
+        print("Please specify --forest and/or --nonforest")
+        exit()
+    
+    if args.forest:
+        write_geotiff_patch_from_points(
+            sampleImage,
+            ee_points_forest,
+            ["R", "G", "B", "N"],
+            10,
+            32,
+            output_directory=args.output_dir,
+            suffix="forest",
+        )
+
+    if args.nonforest:
+        write_geotiff_patch_from_points(
+            sampleImage,
+            ee_points_nonforest,
+            ["R", "G", "B", "N"],
+            10,
+            32,
+            output_directory=args.output_dir,
+            suffix="nonforest",
+        )
+
+
+if __name__ == "__main__":
+    main()
--- fao_models/serving.py
+++ fao_models/serving.py
@@ -11,6 +11,117 @@ import requests
 
 
 @retry.Retry()
+def get_tiff_patch_url_file_point_least_cloudy(
+    imageColl: ee.ImageCollection,
+    point: ee.Geometry,
+    bands: list,
+    scale: int,
+    patch_size: int,
+    output_file: str,
+):
+    """
+    Return ee.Image.getDownloadURL response and a filename as a tuple for a GeoTIFF. filename is passed through for concurrent.futures multiprocessing jobs. Uses points rather than polygons.
+    args:
+      imageColl: ee.ImageCollection
+      point: ee.Geometry,
+      bands: list,
+      scale: int,
+      patch_size: int,
+      output_file: str,
+    """
+    # Create the URL to download the band values of the patch of pixels.
+    point = ee.Geometry(point)
+    region = point.buffer(scale * patch_size / 2, 1).bounds(1)
+    
+    # filter s2 collection on the fly per point to get least cloudy image
+    image = imageColl.filterBounds(point).sort("CLOUDY_PIXEL_PERCENTAGE").first()
+    
+    url = image.getDownloadURL(
+        {
+            "region": region,
+            "dimensions": [patch_size, patch_size],
+            "format": "GEO_TIFF",
+            "bands": bands,
+        }
+    )
+    response = requests.get(url)
+    if response.status_code == 429:
+        raise exceptions.TooManyRequests(response.text)
+    response.raise_for_status()
+    with open(output_file, "wb") as fd:
+        fd.write(response.content)
+    return 1
+
+def write_geotiff_patch_from_points_least_cloudy(
+    image,
+    points,
+    bands,
+    scale,
+    patch_size,
+    output_directory,
+    suffix=None,
+    num_workers=10,
+):
+    """Writes patches inside boxes a GEE Image within a FeatureCollection of boxes to individual GeoTIFFs
+    args:
+      image: ee.Image 
+      points: ee.FeatureCollection
+      bands: list(str)
+      scale: int
+      patch_size: int
+      output_directory: str
+      suffix: str
+      num_workers: int
+    """
+    with concurrent.futures.ThreadPoolExecutor(
+        max_workers=num_workers
+    ) as executor:  # max concurrent requests to high volume endpoint is 40
+        # convert points FeatureCollection to ee.Geomtry's
+        patch_pt_list = (
+            points.toList(points.size())
+            .map(lambda f: ee.Feature(f).geometry())
+            .getInfo()
+        )  # list of ee.Geometry's
+        patch_pt_list_filenames = [
+            os.path.join(output_directory, f"patch_pt{list_index}_{suffix}.tif")
+            for list_index in list(range(0, points.size().getInfo()))
+        ]  # list of filenames
+        pt_filename = zip(patch_pt_list, patch_pt_list_filenames)
+        # # don't write patches that already exist on disk
+        pt_filename = [pf for pf in pt_filename if not os.path.exists(pf[1])]
+        # end this stuff
+        futures = {
+            executor.submit(
+                get_tiff_patch_url_file_point_least_cloudy,
+                image,
+                pt,
+                bands,
+                scale,
+                patch_size,
+                filename,
+            ): (pt, filename)
+            for (
+                pt,
+                filename,
+            ) in pt_filename  # zip(patch_pt_list,patch_pt_list_filenames)
+        }
+        try:
+            for future in concurrent.futures.as_completed(futures, timeout=120):
+                try:
+                    # get result of future with timeout of 120s
+                    result = future.result(timeout=120)
+
+                except concurrent.futures.TimeoutError:
+                    print("The task exceeded 2-minute limit and was cancelled")
+                except Exception as e:
+                    print(f"Loop - Generated an exception: {e}")
+        except Exception as e:
+            # TODO - this still generates an exception when doing a long running (and finishing task)
+            #        Test to see if removing try/except will still allow task to finish.
+            print(f"Outer - Generated an exception: {e}")
+
+
+@retry.Retry()
 def get_tiff_patch_url_file_point(
     image: ee.Image,
     point: ee.Geometry,
@@ -48,7 +159,7 @@ def get_tiff_patch_url_file_point(
     return 1
 
 
-def write_geotiff_patch_from_points_v2(
+def write_geotiff_patch_from_points(
     image,
     points,
     bands,
@@ -60,14 +171,14 @@ def write_geotiff_patch_from_points_v2(
 ):
     """Writes patches inside boxes a GEE Image within a FeatureCollection of boxes to individual GeoTIFFs
     args:
-      image: ee.Image
+      image: ee.Image 
       points: ee.FeatureCollection
       bands: list(str)
       scale: int
       patch_size: int
       output_directory: str
       suffix: str
-
+      num_workers: int
     """
     with concurrent.futures.ThreadPoolExecutor(
         max_workers=num_workers
@@ -115,54 +226,6 @@ def write_geotiff_patch_from_points_v2(
             # TODO - this still generates an exception when doing a long running (and finishing task)
             #        Test to see if removing try/except will still allow task to finish.
             print(f"Outer - Generated an exception: {e}")
-
-
-def write_geotiff_patch_from_points(
-    image, points, bands, scale, patch_size, output_directory, suffix=None
-):
-    """Writes patches inside boxes a GEE Image within a FeatureCollection of boxes to individual GeoTIFFs
-    args:
-      image: ee.Image
-      points: ee.FeatureCollection
-      bands: list(str)
-      scale: int
-      patch_size: int
-      output_directory: str
-      suffix: str
-
-    """
-    EXECUTOR = concurrent.futures.ThreadPoolExecutor(
-        max_workers=40
-    )  # max concurrent requests to high volume endpoint
-
-    # convert points FeatureCollection to ee.Geomtry's
-    patch_pt_list = (
-        points.toList(points.size()).map(lambda f: ee.Feature(f).geometry()).getInfo()
-    )  # list of ee.Geometry's
-    patch_pt_list_filenames = [
-        os.path.join(output_directory, f"patch_pt{list_index}_{suffix}.tif")
-        for list_index in list(range(0, points.size().getInfo()))
-    ]  # list of filenames
-
-    pt_filename = zip(patch_pt_list, patch_pt_list_filenames)
-
-    # don't write patches that already exist on disk
-    pt_filename = [pf for pf in pt_filename if not os.path.exists(pf[1])]
-
-    future_to_point = {
-        EXECUTOR.submit(
-            get_tiff_patch_url_file_point, image, pt, bands, scale, patch_size, filename
-        ): (pt, filename)
-        for (pt, filename) in zip(patch_pt_list, patch_pt_list_filenames)
-    }
-
-    for future in concurrent.futures.as_completed(future_to_point):
-        result = future.result()
-        resp = result[0]
-        filename = result[1]
-        with open(filename, "wb") as fd:
-            fd.write(resp.content)
-
 
 def write_tfrecord_batch(image, patch_size, points, scale, output_file):
     """Writes patches at a set of points to a TFRecord file, using ee.data.ComputePixels
