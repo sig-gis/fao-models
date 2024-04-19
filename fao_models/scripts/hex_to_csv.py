f42b26820ed423b81b352a4af14a0896da8d3b1c
John Dilger <johnjdilger@gmail.com>
2024-04-19 13:34:36 -0500

    download v2

--- fao_models/scripts/hex_to_csv.py
+++ fao_models/scripts/hex_to_csv.py
@@ -1,6 +1,8 @@
 import ee
 import requests
 
+from clean_csv import *
+
 PROJECT = "pc530-fao-fra-rss"
 ee.Initialize(project=PROJECT)
 
@@ -9,10 +11,10 @@ hex = ee.FeatureCollection(
 )
 hex_forest = hex.filter(
     ee.Filter.And(ee.Filter.eq("FOREST", 1), ee.Filter.eq("LU18CEN", "Forest"))
-)
+).map(lambda f: f.set("label", 1))
 hex_nonforest = hex.filter(
     ee.Filter.And(ee.Filter.eq("FOREST", 0), ee.Filter.neq("LU18CEN", "Forest"))
-)
+).map(lambda f: f.set("label", 0))
 hex_samples = (
     ee.FeatureCollection([hex_forest, hex_nonforest]).flatten().randomColumn(seed=42)
 )
@@ -31,11 +33,19 @@ print(hex_samples.limit(1).getInfo())
 training_sample = hex_samples.filter("random <= 0.7")
 testing_sample = hex_samples.filter("random > 0.7 and random <=.9")
 validation_sample = hex_samples.filter("random > 0.9")
+
+
 # print(
 #     training_sample.size().getInfo(),
 #     testing_sample.size().getInfo(),
 #     validation_sample.size().getInfo(),
 # )
+def downloadv2(collection: ee.FeatureCollection, full_namepath: str):
+    url = collection.getDownloadURL()
+    df = load_csv(url)
+    add_new_index(df)
+    df = select_columns(df, ["id", "lng", "lat", "label"])
+    save(df, f"match_{full_namepath}")
 
 
 def download(collection: ee.FeatureCollection, full_namepath: str):
@@ -46,6 +56,6 @@ def download(collection: ee.FeatureCollection, full_namepath: str):
         fd.write(response.content)
 
 
-download(training_sample, "training_sample.csv")
-download(testing_sample, "testing_sample.csv")
-download(validation_sample, "validation_sample.csv")
+downloadv2(training_sample, "training_sample.csv")
+downloadv2(testing_sample, "testing_sample.csv")
+downloadv2(validation_sample, "validation_sample.csv")
